01861d04de0fcf8187870839ce755ab696ab49c8
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-02-08 17:21:11 +0530

    second commit

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -3,38 +3,36 @@ const nodemailer = require('nodemailer');
 const router = express.Router();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // You can use any email service you prefer
+    service: 'gmail',
     auth: {
-      user: process.env.fromEmail, // Replace with your email
-      pass: process.env.googleAppPassword   // Replace with your email password or app password if using 2FA
+      user: process.env.fromEmail,
+      pass: process.env.googleAppPassword
     }
 });
 
 router.post("/", (req, res) => {
-    const {name, email, message} = req.body;
+    const { name, email, message } = req.body;
+
+    console.log('Contact data received:', { name, email, message });
 
     const mailOptions = {
         from: email,
-        to: process.env.fromEmail, // Replace with your email
+        to: process.env.fromEmail,
         subject: `New message from ${name}`,
         text: `You have a new message from:
         Name: ${name}
         Email: ${email}
         Message: ${message}`
     };
-    
-    transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Error sending email' });
-    } else {
-        console.log('Email sent: ' + info.response);
-        res.status(200).json({ message: 'Email sent successfully' });
-    }
-    });
 
-    console.log('Contact data received:', { name, email, message });
-    res.status(200).json({ message: 'Contact data received successfully' });
-})
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log("Email error:", error);
+            return res.status(500).json({ message: 'Error sending email' });
+        } 
+        console.log('Email sent:', info.response);
+        res.status(200).json({ message: 'Email sent successfully' });
+    });
+});
 
 module.exports = router;
